4faba8804010b314254c7d0370cd43362ae2db93
Nethelbert <gilblasse@gmail.com>
2019-12-15 06:21:47 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
   
   def list_roles
     binding.pry
-    #{self.characters.first.name} - #{ }
+    #{self.characters.first.name} - #{self.characters.first.show}
   end
   
 end
